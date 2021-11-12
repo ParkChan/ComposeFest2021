c9fa8926e049443510146de0d709369925fe6b29
ParkChan <hydrarkk@naver.com>
2021-11-12 17:43:54 +0900

    [Codlab/Step] 2. Starting a new Compose project

---
+++ week 2-1-Layouts in JetpackCompose/Layoutscodelabs_Chan/app/src/main/java/com/chan/layoutscodelabs/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.chan.layoutscodelabs
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import com.chan.layoutscodelabs.ui.theme.LayoutsCodelabTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            LayoutsCodelabTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(color = MaterialTheme.colors.background) {
+
+                }
+            }
+        }
+    }
+}
