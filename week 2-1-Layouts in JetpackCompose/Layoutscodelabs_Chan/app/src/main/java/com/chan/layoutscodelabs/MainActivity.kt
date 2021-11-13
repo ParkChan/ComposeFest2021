94e686fbe735d7281166d57e237fa1a6ec03cda0
ParkChan <hydrarkk@naver.com>
2021-11-14 01:52:34 +0900

    [Codelab/Step] 7. Create your custom layout

--- week 2-1-Layouts in JetpackCompose/Layoutscodelabs_Chan/app/src/main/java/com/chan/layoutscodelabs/MainActivity.kt
+++ week 2-1-Layouts in JetpackCompose/Layoutscodelabs_Chan/app/src/main/java/com/chan/layoutscodelabs/MainActivity.kt
@@ -12,7 +12,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             LayoutsCodelabTheme {
-                ScrollingList()
+
             }
         }
     }
@@ -21,5 +21,5 @@ class MainActivity : ComponentActivity() {
 @Preview
 @Composable
 fun Preview() {
-    ScrollingListPreview()
+    MyOwnColumnPreview()
 }
